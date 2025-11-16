3f80cf649cecd9c72ecc94d5dc71ec70efc1e195
oleg_eremichev <eremuchew.gelo@gmail.com>
2025-11-17 01:12:34 +0300

    Написана первая версия статьи

--- firstTask.py
+++ firstTask.py
@@ -3,6 +3,7 @@ from enum import Enum, auto
 import numpy as np
 import matplotlib.pyplot as plot
 from mpl_toolkits.mplot3d import Axes3D
+from scipy.optimize import fsolve
 
 class Solver:
 	def __init__(self, lmda, sigma, gamma):
@@ -16,25 +17,33 @@ class Solver:
 		self.delta = 0.01
 		self.tauMax = 100
 		self.xInterval = (0, 2 * np.pi)
-		self.xi1Interval = (-1.2, 1.2)
+		self.reXiInterval = (-1.2, 1.2)
 		self.tauPointsCount = int(self.tauMax / self.delta) + 1
 		self.xPointsCount = int(self.xInterval[1] / self.h) + 1
-		self.xi1Values = np.zeros((self.tauPointsCount, self.xPointsCount))
-		self.xi2Values = np.zeros((self.tauPointsCount, self.xPointsCount))
+		self.reXiValues = np.zeros((self.tauPointsCount, self.xPointsCount))
+		self.imXiValues = np.zeros((self.tauPointsCount, self.xPointsCount))
 
-	def reXiFunction1()
-		
+	def equations(self, variables):
+		x, y = variables
+		eq1 = x ** 2 + y ** 2 - 4
+		eq2 = x + y + 8
+		return [eq1, eq2]
+
+	# def reXiFunction1()
+
+	def function1(self, rho1, rho2, alpha, betta , x, tau, phi):
+		multiplier = np.exp(1j * betta * tau)
+		if x <= alpha:
+			return rho1 * multiplier + np.cos(20 * x) / 100
+		else:
+			return rho2 * np.exp(phi * 1j) + + np.cos(20 * x) / 100
 
 	def setInitialCondition(self):
-		xAllValues = np.arange(self.xInterval[0], self.xInterval[1] / 2, self.h)
-		yAllValues = np.sin(20 * xAllValues) / 40 - 1
-		xAllValues = np.concatenate([xAllValues, np.array([xAllValues[-1] + self.h])])
-		yAllValues = np.concatenate([yAllValues, np.array([np.sin(20 * (xAllValues[-1])) / 40 + 1])])
-		x = np.arange(xAllValues[-1] + self.h, self.xInterval[1], self.h)
-		y = np.sin(20 * x) / 40 + 1
-		xAllValues = np.concatenate([xAllValues, x])
-		yAllValues = np.concatenate([yAllValues, y])
-		self.xi1Values[0][:] = yAllValues
+		xValues = np.arange(self.xInterval[0], self.xInterval[1], self.h)
+		for i, x in enumerate(xValues):
+			yValue = self.function1(-1, 1, np.pi, 2, x, 0, 0)
+			self.reXiValues[0][i] = np.real(yValue)
+			self.imXiValues[0][i] = np.imag(yValue)
 	
 	def integral(self, tauIndex, functionValues):
 		value = 0
@@ -43,81 +52,86 @@ class Solver:
 		return value
 
 	def g(self, tauIndex, xIndex):
-		xi1 = self.xi1Values[tauIndex][xIndex]
-		xi2 = self.xi2Values[tauIndex][xIndex]
-		return self.lmda1 * xi1 + self.lmda0 * xi2 + self.sigma0 * xi2 * xi1 * xi1 \
-			+ self.sigma1 * xi1 * xi1 * xi1 + self.sigma0 * xi2 * xi2 * xi2 \
-			+ self.sigma1 * xi1 * xi2 * xi2 + self.gamma0 * self.integral(tauIndex, self.xi2Values) \
-			+ self.gamma1 * self.integral(tauIndex, self.xi1Values)
+		reXi = self.reXiValues[tauIndex][xIndex]
+		imXi = self.imXiValues[tauIndex][xIndex]
+		return self.lmda1 * reXi + self.lmda0 * imXi + self.sigma0 * imXi * reXi * reXi \
+			+ self.sigma1 * reXi * reXi * reXi + self.sigma0 * imXi * imXi * imXi \
+			+ self.sigma1 * reXi * imXi * imXi + self.gamma0 * self.integral(tauIndex, self.imXiValues) \
+			+ self.gamma1 * self.integral(tauIndex, self.reXiValues)
 
 	def f(self, tauIndex, xIndex):
-		xi1 = self.xi1Values[tauIndex][xIndex]
-		xi2 = self.xi2Values[tauIndex][xIndex]
-		return -self.lmda1 * xi2 + self.lmda0 * xi1 + self.sigma0 * xi1 * xi1 * xi1 \
-			+ self.sigma1 * xi2 * xi1 * xi1 + self.sigma0 * xi1 * xi2 * xi2 \
-			- self.sigma1 * xi1 * xi1 * xi1 + self.gamma0 * self.integral(tauIndex, self.xi1Values) \
-			- self.gamma1 * self.integral(tauIndex, self.xi2Values)
+		reXi = self.reXiValues[tauIndex][xIndex]
+		imXi = self.imXiValues[tauIndex][xIndex]
+		return -self.lmda1 * imXi + self.lmda0 * reXi + self.sigma0 * reXi * reXi * reXi \
+			+ self.sigma1 * imXi * reXi * reXi + self.sigma0 * reXi * imXi * imXi \
+			- self.sigma1 * reXi * reXi * reXi + self.gamma0 * self.integral(tauIndex, self.reXiValues) \
+			- self.gamma1 * self.integral(tauIndex, self.imXiValues)
 
 	def drawGraph(self):
-		plot.figure(figsize=(6, 4))
 
-		plot.subplot(2, 2, 1)
-		self.setInitialCondition()
-		plot.title(r"$\xi_1$(0, $x$)")
-		plot.ylabel(r"$\xi_1$")
-		plot.xlabel(r"$x$")
-		plot.xlim(self.xInterval[0], self.xInterval[1])
-		plot.ylim(self.xi1Interval[0], self.xi1Interval[1])
-		plot.grid(True)
-		x = np.linspace(self.xInterval[0], self.xInterval[1], len(self.xi1Values[0]))
-		plot.plot(x, self.xi1Values[0])
+		solution = fsolve(self.equations, [1, 1])
+		print("Solution to the system:", solution)
+
+		# plot.figure(figsize=(6, 4))
+
+		# plot.subplot(1, 2, 1)
+		# self.setInitialCondition()
+		# plot.title(r"$\xi_1$(0, $x$)")
+		# plot.ylabel(r"$\xi_1$")
+		# plot.xlabel(r"$x$")
+		# plot.xlim(self.xInterval[0], self.xInterval[1])
+		# plot.ylim(self.reXiInterval[0], self.reXiInterval[1])
+		# plot.grid(True)
+		# x = np.linspace(self.xInterval[0], self.xInterval[1], len(self.reXiValues[0]))
+		# plot.plot(x, self.reXiValues[0])
+
+		# # calculateCount = 10
+		# # for tau in range(calculateCount):
+		# # 	for x in range(self.xPointsCount):
+		# # 		self.imXiValues[tau + 1][x] = self.imXiValues[tau][x] + self.delta * self.g(tau, x)
+		# # 		self.reXiValues[tau + 1][x] = self.reXiValues[tau][x] + self.delta * self.f(tau, x)
+		# # plot.subplot(2, 2, 2)
+		# # plot.title(r"$\xi_1$({}, $x$)".format(calculateCount * self.delta))
+		# # plot.ylabel(r"$\xi_1$")
+		# # plot.xlabel(r"$x$")
+		# # plot.xlim(self.xInterval[0], self.xInterval[1])
+		# # plot.ylim(self.reXiInterval[0], self.reXiInterval[1])
+		# # plot.grid(True)
+		# # x = np.linspace(self.xInterval[0], self.xInterval[1], self.xPointsCount)
+		# # plot.plot(x, self.reXiValues[calculateCount])
+
+		# # calculateCount = 20
+		# # for tau in range(calculateCount):
+		# # 	for x in range(self.xPointsCount):
+		# # 		self.imXiValues[tau + 1][x] = self.imXiValues[tau][x] + self.delta * self.g(tau, x)
+		# # 		self.reXiValues[tau + 1][x] = self.reXiValues[tau][x] + self.delta * self.f(tau, x)
+		# # plot.subplot(2, 2, 3)
+		# # plot.title(r"$\xi_1$({}, $x$)".format(calculateCount * self.delta))
+		# # plot.ylabel(r"$\xi_1$")
+		# # plot.xlabel(r"$x$")
+		# # plot.xlim(self.xInterval[0], self.xInterval[1])
+		# # plot.ylim(self.reXiInterval[0], self.reXiInterval[1])
+		# # plot.grid(True)
+		# # x = np.linspace(self.xInterval[0], self.xInterval[1], self.xPointsCount)
+		# # plot.plot(x, self.reXiValues[calculateCount])
 
 		# calculateCount = 10
 		# for tau in range(calculateCount):
 		# 	for x in range(self.xPointsCount):
-		# 		self.xi2Values[tau + 1][x] = self.xi2Values[tau][x] + self.delta * self.g(tau, x)
-		# 		self.xi1Values[tau + 1][x] = self.xi1Values[tau][x] + self.delta * self.f(tau, x)
-		# plot.subplot(2, 2, 2)
-		# plot.title(r"$\xi_1$({}, $x$)".format(calculateCount * self.delta))
-		# plot.ylabel(r"$\xi_1$")
-		# plot.xlabel(r"$x$")
-		# plot.xlim(self.xInterval[0], self.xInterval[1])
-		# plot.ylim(self.xi1Interval[0], self.xi1Interval[1])
-		# plot.grid(True)
-		# x = np.linspace(self.xInterval[0], self.xInterval[1], self.xPointsCount)
-		# plot.plot(x, self.xi1Values[calculateCount])
-
-		# calculateCount = 20
-		# for tau in range(calculateCount):
-		# 	for x in range(self.xPointsCount):
-		# 		self.xi2Values[tau + 1][x] = self.xi2Values[tau][x] + self.delta * self.g(tau, x)
-		# 		self.xi1Values[tau + 1][x] = self.xi1Values[tau][x] + self.delta * self.f(tau, x)
-		# plot.subplot(2, 2, 3)
-		# plot.title(r"$\xi_1$({}, $x$)".format(calculateCount * self.delta))
-		# plot.ylabel(r"$\xi_1$")
-		# plot.xlabel(r"$x$")
-		# plot.xlim(self.xInterval[0], self.xInterval[1])
-		# plot.ylim(self.xi1Interval[0], self.xi1Interval[1])
-		# plot.grid(True)
-		# x = np.linspace(self.xInterval[0], self.xInterval[1], self.xPointsCount)
-		# plot.plot(x, self.xi1Values[calculateCount])
-
-		# calculateCount = 30
-		# for tau in range(calculateCount):
-		# 	for x in range(self.xPointsCount):
-		# 		self.xi2Values[tau + 1][x] = self.xi2Values[tau][x] + self.delta * self.g(tau, x)
-		# 		self.xi1Values[tau + 1][x] = self.xi1Values[tau][x] + self.delta * self.f(tau, x)
-		# plot.subplot(2, 2, 4)
+		# 		self.imXiValues[tau + 1][x] = self.imXiValues[tau][x] + self.delta * self.g(tau, x)
+		# 		self.reXiValues[tau + 1][x] = self.reXiValues[tau][x] + self.delta * self.f(tau, x)
+		# plot.subplot(1, 2, 2)
 		# plot.title(r"$\xi_1$({}, $x$)".format(calculateCount * self.delta))
 		# plot.grid(True)
 		# plot.ylabel(r"$\xi_1$")
 		# plot.xlabel(r"$x$")
 		# plot.xlim(self.xInterval[0], self.xInterval[1])
-		# plot.ylim(self.xi1Interval[0], self.xi1Interval[1])
+		# plot.ylim(self.reXiInterval[0], self.reXiInterval[1])
 		# x = np.linspace(self.xInterval[0], self.xInterval[1], self.xPointsCount)
-		# plot.plot(x, self.xi1Values[calculateCount])
+		# plot.plot(x, self.reXiValues[calculateCount])
+		# print(self.imXiValues[calculateCount][:10])
 
-		plot.show()
+		# plot.show()
 
 if __name__ == "__main__":
 	Solver(1, 1, -4).drawGraph()
